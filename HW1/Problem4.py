59fadf5c39d06f42054a2633ed1b939a2bb0d85d
MrHagram <khachieubn2411@gmail.com>
2020-09-19 02:25:36 +0700

    Homework 1

---
+++ HW1/Problem4.py
@@ -0,0 +1,2 @@
+total_sales = int(input("Enter amount of total sales: "))
+print("The profit: ", str(total_sales*23/100))
---
+++ HW1/Problem6.py
@@ -0,0 +1,8 @@
+def distance(time):
+
+    print("The distance the car will travel in {:d} is".format(time), time*70)
+
+
+distance(6)
+distance(10)
+distance(15)
