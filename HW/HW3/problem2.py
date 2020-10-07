421582ce56355295db59b561a9a174ed7c522990
hieu <khachieubn2411@gmail.com>
2020-10-07 09:49:38 +0700

    update

--- HW/HW3/problem2.py
+++ HW/HW3/problem2.py
@@ -6,16 +6,32 @@ def find_x(a, b, n):
     while True:
         dividend_arr.append(n)
         divisor_arr.append(a)
-        quotient_arr.append(int(n/a))
+        quotient_arr.append(-int(n/a))
         remainder_arr.append(n%a)
         remainder = n % a
         n = a
         a = remainder
         if(remainder == 0):
             break
-    for i in range(1,len(dividend_arr)+1):
-        string = str(remainder_arr[-i]) + " = " + str(dividend_arr[-i]) + " - " + str(divisor_arr[-i]) + " *"+ str(quotient_arr[-i])
+    remainder_string_arr = list()
+    for i in range(len(dividend_arr)):
+        string = str(remainder_arr[i]) + " = " + str(dividend_arr[i]) + " + " + str(divisor_arr[i]) + " *"+ "("+str(quotient_arr[i])+")"
+        remainder_string_arr.append(string)
         print(string)
+    print(remainder_string_arr)
+    s_arr = [1 for i in range(len(quotient_arr))]
+    r_arr = list()
+    tmp = [1 for i in range(len(quotient_arr))]
+
+    for i in range(len(quotient_arr)):
+        r_arr.append(quotient_arr[i])
+    for i in range(3,len(quotient_arr)+1):
+        s_arr[-i] = r_arr[-(i-1)]
+
+        r_arr[-i] = r_arr[-i] * r_arr[-(i-1)] + s_arr[-(i-1)]
+
+    print(s_arr)
+    print(r_arr)
 
 def main():
     a = 28
