bb3a71d9e69d69025d0b1027944d87f239583c82
MrHagram <khachieubn2411@gmail.com>
2020-10-09 09:55:55 +0700

    driver-license problem

--- HW/HW3/problem1.py
+++ HW/HW3/problem1.py
@@ -1,30 +1,71 @@
-def driver_license_exam():
+#function create_question can be use to create a text file to store all question and answer
+def create_question():
+        question_file = open("question.txt","w+")
+        correct_answer = open("correct_answer.txt","w+")
+        question_number = 1
+        #ask user to put question, answer and correct answer
+        #this text cannot have more than 20 question
+        while question_number <= 20:
+                question = input("Enter the question: ")
+                question_file.writelines(str(question_number)+'. '+question + '\n')
+                question_number +=1
+                question_file.writelines('A.' + str(input("enter the answer A."))+ '\n')
+                question_file.writelines('B.' + str(input("enter the answer B."))+ '\n')
+                question_file.writelines('C.' + str(input("enter the answer C."))+ '\n')
+                question_file.writelines('D.' + str(input("enter the answer D."))+ '\n')
+                #Ask user to add correct answer then store it in a text file
+                correct_answer.writelines(str(input("Enter the correct answer: ")))
+                continue_box = input("continue? (y/n)")
+                if(continue_box.lower().__contains__("n")):
+                        break
+#function load question loads a text file containing question then return a list of question and answer
+def load_question():
+        question_file = open("question.txt", "r")
+        questions = question_file.readlines()
+        return questions
+#function do test print question and answer then ask user put their answer then store in a text file
+def do_test():
+        answer_file = open("myanswer.txt","w+")
+        questions = load_question()
+        print(len(questions))
+        print(questions[5])
+        for question_no in range(len(questions)):
+                print(questions[question_no])
+                if(question_no % 5 == 4):
+                        answer = str(input("Enter your answer: "))
+                        answer_file.write(str(question_no) + '.' + answer)
+        return answer_file
+
+#function mark_answer read 2 file, first one is the file contain answer of examiner other contain correct answer
+#then calculate score based on correct answer
+def mark_answer():
         count = 0
-        correct_answer = ['A','C','A','A','D','B','C','A','C','B','A','D','C','A','D','C','B','B','D','A']
-        answer_file = open("answer.txt","r+")
-        answer_string = answer_file.read()
-        print(answer_string)
-        answer_list = []
-        wrong_answer_list = []
-        for i in range(len(answer_string)):
-                if answer_string[i] == '.' or answer_string[i] == '. ':
-                        answer_list.append(answer_string[i+1])
-        print(answer_list)
-        for i in range(len(answer_list)):
-                if answer_list[i] == correct_answer[i]:
-                        count += 1
-                else:
-                        wrong_answer_list.append(str(i+1))
-        if(count >= 15):
-                print("Passed!")
-                print("Score: "+count)
-        print("Total number of correct answer is ", count, "and the total of wrong answer is",str(20-count))
-        print("List of question that incorrect: ", wrong_answer_list)
+        answer_file = open("myanswer.txt","r")
+        correct_answer_file = open("correct_answer.txt","r")
+        answers = answer_file.readlines()
+        correct_answer = correct_answer_file.readlines()
+        for i in range(len(answers)):
+                if answers[i] == correct_answer[i]:
+                       count += 1
+        mark = float(count/20 * 10)
+        print("Your mark: {0:3.2f}".format(mark))
+
+
+
+
+
+
 
 
 def main():
-        string = '1    2 5 124'
-        print(string.split(' '))
-        driver_license_exam()
+        create_question()
+        do_test()
+        mark_answer()
+
 
 main()
+
+
+
+
+
