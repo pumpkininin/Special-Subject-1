1f822d19614975053f9bb98408d22fd654e13850
MrHagram <khachieubn2411@gmail.com>
2020-10-09 07:35:06 +0700

    euclidean algorithm w/o comment

--- HW/HW3/problem2.py
+++ HW/HW3/problem2.py
@@ -1,4 +1,5 @@
 def find_x(a, b, n):
+    sub_n = n
     divisor_arr = list()
     dividend_arr = list()
     quotient_arr = list()
@@ -13,12 +14,7 @@ def find_x(a, b, n):
         a = remainder
         if(remainder == 0):
             break
-    remainder_string_arr = list()
-    for i in range(len(dividend_arr)):
-        string = str(remainder_arr[i]) + " = " + str(dividend_arr[i]) + " + " + str(divisor_arr[i]) + " *"+ "("+str(quotient_arr[i])+")"
-        remainder_string_arr.append(string)
-        print(string)
-    print(remainder_string_arr)
+    gcd = remainder_arr[-2]
     s_arr = [1 for i in range(len(quotient_arr))]
     r_arr = list()
     tmp = [1 for i in range(len(quotient_arr))]
@@ -27,17 +23,30 @@ def find_x(a, b, n):
         r_arr.append(quotient_arr[i])
     for i in range(3,len(quotient_arr)+1):
         s_arr[-i] = r_arr[-(i-1)]
-
         r_arr[-i] = r_arr[-i] * r_arr[-(i-1)] + s_arr[-(i-1)]
+    result = list()
+    x0 = int(r_arr[0]*b/gcd)
+    while True:
+        x0 += int(dividend_arr[0]/gcd)
+        if(x0 > 0 and x0 < dividend_arr[0]):
+            result.append(x0)
+        else:
+            break
 
-    print(s_arr)
-    print(r_arr)
-
+    print(result)
+    print("dividend  | divisor  | quotient | remainder")
+    for i in range(len(dividend_arr)):
+        print("{0:10d}|{1:10d}|{2:10d}|{3:10d}".format(dividend_arr[i], divisor_arr[i], quotient_arr[i], remainder_arr[i]))
+    print("GCD:",gcd)
+    for i in range(2,len(dividend_arr)+1):
+        print("step {5:3d}: {0:3d} = {1:3d}*({2:3d}) + {3:3d}*({4:3d})".format(gcd, dividend_arr[-i],s_arr[-i], divisor_arr[-i], r_arr[-i],i-1))
+    print("X0 = {0:3d}*{1:3d}/{2:3d} = {3:3d} (mod {4:3d}/{2:3d})".format(r_arr[0],b,gcd, result[0],dividend_arr[0]))
+    print("All posibble solution: ", result)
 def main():
     a = 28
     b = 8
     n = 48
-    print(find_x(a,b,n))
+    find_x(a,b,n)
 
 
 main()
